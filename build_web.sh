14147f927586cf2fa0e77195f2a291f385dcaffa
spooky-th-ghost <colin@wildheartinteractive.com>
2025-08-03 14:20:51 -0400

    update web build script

--- build_web.sh
+++ build_web.sh
@@ -2,7 +2,7 @@
 
 # Point this to where you installed emscripten. Optional on systems that already
 # have `emcc` in the path.
-EMSCRIPTEN_SDK_DIR="$HOME/tools/emsdk"
+EMSCRIPTEN_SDK_DIR="$HOME/emsdk"
 OUT_DIR="dist/web"
 
 mkdir -p $OUT_DIR
@@ -27,7 +27,7 @@ files="$OUT_DIR/game.o ${ODIN_PATH}/vendor/raylib/wasm/libraylib.a ${ODIN_PATH}/
 
 # index_template.html contains the javascript code that calls the procedures in
 # source/main_web/main_web.odin
-flags="-sUSE_GLFW=3 -sWASM_BIGINT -sWARN_ON_UNDEFINED_SYMBOLS=0 -sASSERTIONS --shell-file web/index_template.html --preload-file assets"
+flags="-sUSE_GLFW=3 -sWASM_BIGINT -sWARN_ON_UNDEFINED_SYMBOLS=0 -sEXPORTED_RUNTIME_METHODS=[\"HEAPF32\"] -sASSERTIONS --shell-file web/index_template.html --preload-file assets"
 
 # For debugging: Add `-g` to `emcc` (gives better error callstack in chrome)
 emcc -o $OUT_DIR/index.html $files $flags
